febc50e5e4864a475d2365509ae3550428c9d015
vedatsengul <vedatsengul@gmail.com>
2020-10-28 11:35:04 +0000

    Initial Commit
    Initial Commit

---
+++ RedBadger/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RedBadger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
